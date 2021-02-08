23b5d5d33ecdde7b231d79756a97345fffc193e4
Garin Jankowski <garin.jankowski@yahoo.com>
2021-02-08 15:25:57 -0800

    Added the starter stuff. Javafx window opens with a Menu Bar and Text Area.

---
+++ src/Controller.java
@@ -0,0 +1,10 @@
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class Controller {
+    @FXML
+    public VBox vbox;
+
+    @FXML public void initialize(){
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Text Editor");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.show();
+    }
+}
