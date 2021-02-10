7bed04a907bfa7db571c4db8ab0947b088b4459d
Garin Jankowski <garin.jankowski@yahoo.com>
2021-02-10 13:48:19 -0800

    Added a skeleton for Originator.

--- src/MementoPattern/Caretaker.java
+++ src/MementoPattern/Caretaker.java
@@ -1,4 +1,17 @@
 package MementoPattern;
 
+import java.util.LinkedList;
+
 public class Caretaker {
+    LinkedList<Memento> undos;
+    LinkedList<Memento> redos;
+
+    public Caretaker(){
+        undos = new LinkedList<>();
+        redos = new LinkedList<>();
+    }
+
+    public void addUndo(Memento undo){
+        undos.addFirst(undo);
+    }
 }
--- src/MementoPattern/Originator.java
+++ src/MementoPattern/Originator.java
@@ -1,4 +1,17 @@
 package MementoPattern;
 
 public class Originator {
+    private String currentState;
+
+    public String getCurrentState(){
+        return currentState;
+    }
+
+    public Memento saveState(){
+        return new Memento(currentState);
+    }
+
+    public void setState(Memento memento){
+        currentState = memento.getState();
+    }
 }
